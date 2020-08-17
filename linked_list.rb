e4005ed014c5758b8e8639f51161adb7bb6b1bcd
JoeyJr <JoeyJr11@gmail.com>
2020-08-17 15:18:26 -0500

    updated linked list

--- linked_list.rb
+++ linked_list.rb
@@ -1,11 +1,12 @@
 class LinkedList
     attr_accessor :head, :tail
     def initialize
-        @head = null
-        @tail = null
+        @head = nil
+        @tail = nil
     end
 
     class Node
+        attr_accessor :value, :next_node
         def initialize(value)
             @value = value
             @next_node = nil
@@ -13,25 +14,41 @@ class LinkedList
     end
 
     def print_list
-        puts 
+        if @head == nil
+            puts "List is empty"
+        elsif @head == @tail
+            puts "List: ( #{@head.value} )"
+        else
+            puts "List: ( #{@head.value} ), 
+                then: ( #{@head.next_node.value} ), 
+                tail: ( #{@tail.value} )"
+        end
     end
     #code to add node to the end of the list
     def append(value)
         new_node = Node.new(value)
-        if @head == null
+        if @head == nil
             @head = new_node
-            @tail = new_node.next_node
-        else
             @tail = new_node
-            new_node.next_node = nil
+            @head.next_node = @tail
+        else 
+            @tail = new_node        
         end
     end
 
+    #code to add node to the beginning of the list
     def prepend(value)
         new_node = Node.new(value)
-        @head = new_node
-        new_node.next_node = 
-        #code to add node to beginning of the list
+        if @head == nil
+            @head = new_node
+            @tail = new_node
+            @head.next_node = @tail
+        else
+            new_node = Node.new(value)
+            temp_node = @head
+            new_node.next_node = @head
+            @head = new_node
+        end
     end
 
     def size
@@ -48,5 +65,8 @@ class LinkedList
 end
 
 list = LinkedList.new
-list.Node.new(3)
-list.append(4)
+puts "The List starts off empty: "
+list.print_list
+puts "Let's use 'prepend' to add '1' to the beginning of the list: "
+list.prepend(1)
+list.print_list
